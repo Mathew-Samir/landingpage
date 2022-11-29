b47237a4aa14beec2a0a8752b37fbdce8cf57048
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2022-11-29 21:32:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+
+
+
+function phones(phone){
+    img.src = phone;
+}
+
+
+function colors(color){
+    container.style.background = color;
+}
+
+
+
+
+

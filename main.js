62e0084c94677f00015622f414d794131696e0e8
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2022-12-05 01:11:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let img = document.querySelector(".img");
 let container = document.querySelector(".container");
 
+if(sessionStorage.length > 0){
+    container.style.background =  sessionStorage.color;
+    img.src =  sessionStorage.phone;
+}
 
 
 function phones(phone){
+    sessionStorage.setItem('phone',phone);
     img.src = phone;
 }
 
 
 function colors(color){
+    sessionStorage.setItem('color',color);
     container.style.background = color;
 }
 
